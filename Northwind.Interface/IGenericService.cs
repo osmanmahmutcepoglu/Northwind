1afdee136c1877f08dca3c7e8f32fa63ea7aa713
osmanmahmutcepoglu <osmanmahmutcepoglu@gmail.com>
2022-04-16 11:58:05 +0300

    Generic Repository

---
+++ Northwind.Interface/IGenericService.cs
@@ -0,0 +1,34 @@
+﻿using Northwind.Entity.IBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Interface
+{
+    public interface IGenericService<T,TDto> where T : IEntityBase where TDto : IDtoBase
+    {
+        //Listeleme
+        IResponse<List<TDto>> GetAll();
+        //Filtreli Listeleme
+        IResponse<List<TDto>> GetAll(Expression<Func<T,bool>> expression);
+        //Getirme
+        IResponse<TDto> Find(int id);
+        //Kaydetme
+        IResponse<TDto> Add(TDto item,bool saveChanges = true);
+        //Async Kaydetme
+        Task<IResponse<TDto>> AddAsync(TDto item, bool saveChanges = true);
+        //Güncelleme
+        IResponse<TDto> Update(TDto item, bool saveChanges = true);
+        //Async Güncelleme
+        Task<IResponse<TDto>> UpdateAsync(TDto item, bool saveChanges = true);
+        //Silme
+        IResponse<bool> DeleteById(int id, bool saveChanges = true);
+        //Async Silme
+        Task<IResponse<TDto>> DeleteByIdAsync(int id, bool saveChanges = true);
+        //Queryable Listeleme
+        IQueryable<TDto> GetQueryable();
+    }
+}
