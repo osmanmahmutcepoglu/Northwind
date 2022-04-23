80d2b289e2ed80e4190e6da90ab16a1940d6f3f9
osmanmahmutcepoglu <osmanmahmutcepoglu@gmail.com>
2022-04-23 10:46:21 +0300

    23.04.2022 UnitOfWok

---
+++ Northwind.Dal/Concrete/Entityframework/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Northwind.Dal.Abstract;
+using Northwind.Dal.Concrete.Repository;
+using Northwind.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Concrete.Entityframework.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        #region Variables
+        DbContext context;
+        IDbContextTransaction transaction;
+        bool dispose;
+        #endregion
+
+        public UnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public bool BeginTransaction()
+        {
+            try
+            {
+                transaction = context.Database.BeginTransaction();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGenericRepository<T> GetRepository<T>() where T : EntityBase
+        {
+            return new GenericRepository<T>(context);
+        }
+
+        public bool RollBackTransaction()
+        {
+            try
+            {
+                transaction.Rollback();
+                transaction = null;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
